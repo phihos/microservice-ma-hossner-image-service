bfa5ed6571977d1ddb90b8d17c2daae0b8d7b221
CAEUser <CAEAddress@gmail.com>
2019-07-17 19:38:28 +0200

    Code regeneration/Model synchronization

--- db/ma-hossner-image-service_create_tables.sql
+++ db/ma-hossner-image-service_create_tables.sql
@@ -1,12 +1,12 @@
 --
--- Database Schema:  
+-- Database Schema:  caeschema
 -- Automatically generated sql script for the service ma-hossner-image-service, created by the CAE.
 -- --------------------------------------------------------
 
 --
 -- Table structure for table Images.
 --
-CREATE TABLE .Images (
+CREATE TABLE caeschema.Images (
   imageData TEXT ,
   id INT ,
 CONSTRAINT id_PK PRIMARY KEY (id)
